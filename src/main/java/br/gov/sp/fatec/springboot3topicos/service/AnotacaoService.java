9135d7e021708b5a3ef73a15039e86c93adec212
Gabriel Coutinho <gabrielcoutinho2011@gmail.com>
2024-03-05 12:59:16 +0000

    Criando o service de anotação

--- src/main/java/br/gov/sp/fatec/springboot3topicos/service/AnotacaoService.java
+++ src/main/java/br/gov/sp/fatec/springboot3topicos/service/AnotacaoService.java
@@ -1,5 +1,7 @@
 package br.gov.sp.fatec.springboot3topicos.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -29,5 +31,9 @@ public class AnotacaoService {
             .buscarUsuarioPorId(anotacao.getId());
         anotacao.setUsuario(usuario);
         return anotacaoRepo.save(anotacao);
+    }
+
+    public List<Anotacao> buscarTodas(){
+        return anotacaoRepo.findAll();
     }
 }
